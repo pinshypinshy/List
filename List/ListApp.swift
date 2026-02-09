de2d3da6d68f304890167781d79b92d7c3ed8786
pinshypinshy <hrisyklit@gmail.com>
2026-02-09 15:17:28 +0900

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by 平石悠生 on 2026/02/09.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
